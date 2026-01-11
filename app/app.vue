46098b6175bbb5ca6d103ef8f577de0d0f1bc1c3
Oleg <ikslprint@gmail.com>
2026-01-11 11:06:24 +0200

    install nuxt ui, add main.css

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <UApp>
+    <UContainer>
+      <h1>Hello Nuxt Template</h1>
+    </UContainer>
+  </UApp>
 </template>
